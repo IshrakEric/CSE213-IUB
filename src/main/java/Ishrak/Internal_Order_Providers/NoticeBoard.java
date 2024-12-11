a192da823326e7ddcee34e71ec63fdc2a85dd375
ISHRAK ARIQ <IshrakAriq@mail.com>
2024-12-12 00:06:02 +0600

    Changes in the FXML and Classes Files for User-2 (Internal_Order_Providers).

---
+++ src/main/java/Ishrak/Internal_Order_Providers/NoticeBoard.java
@@ -0,0 +1,63 @@
+package Ishrak.Internal_Order_Providers;
+
+import javax.swing.table.TableColumn;
+
+public class NoticeBoard {
+    @javafx.fxml.FXML
+    private TableColumn download;
+    @javafx.fxml.FXML
+    private TableColumn serial;
+    @javafx.fxml.FXML
+    private TableColumn publishdate;
+    @javafx.fxml.FXML
+    private TableColumn title;
+
+    public NoticeBoard(TableColumn download, TableColumn serial, TableColumn publishdate, TableColumn title) {
+        this.download = download;
+        this.serial = serial;
+        this.publishdate = publishdate;
+        this.title = title;
+    }
+
+    public TableColumn getDownload() {
+        return download;
+    }
+
+    public void setDownload(TableColumn download) {
+        this.download = download;
+    }
+
+    public TableColumn getSerial() {
+        return serial;
+    }
+
+    public void setSerial(TableColumn serial) {
+        this.serial = serial;
+    }
+
+    public TableColumn getPublishdate() {
+        return publishdate;
+    }
+
+    public void setPublishdate(TableColumn publishdate) {
+        this.publishdate = publishdate;
+    }
+
+    public TableColumn getTitle() {
+        return title;
+    }
+
+    public void setTitle(TableColumn title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "NoticeBoard{" +
+                "download=" + download +
+                ", serial=" + serial +
+                ", publishdate=" + publishdate +
+                ", title=" + title +
+                '}';
+    }
+}
