658c838f42d9485e028cd8ba9d2e1766d7acb475
Rafid Islam <rafidislam529@gmail.com>
2024-12-17 10:57:38 +0600

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,11 @@ module com.midterm_2_2211182.shilpakala_academy {
     requires java.desktop;
     requires java.logging;
 
+    requires javafx.graphics;
 
+    exports com.midterm_2_2211182.shilpakala_academy.Rafid;
+
+    opens com.midterm_2_2211182.shilpakala_academy.Rafid to javafx.fxml;
     opens com.midterm_2_2211182.shilpakala_academy to javafx.fxml;
     exports com.midterm_2_2211182.shilpakala_academy;
 }
